bb12d19556fc6d1c814165df020d2324fe82f9d1
yonirg <yonirabinovicigherman@gmail.com>
2022-09-24 17:19:23 -0300

    Arrume a indentação

--- package/funcoes.py
+++ package/funcoes.py
@@ -6,11 +6,11 @@ import pandas as pd
 # Função que pega todas as músicas do Imagine Dragons
 def pega_musicas():
     musicas = []
-i=1
-while i<=10:
-    page = requests.get(f"https://www.last.fm/pt/music/Imagine+Dragons/+tracks?page={i}")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    container_musicas =soup.find_all('td', class_='chartlist-name')
-    for musica in container_musicas:
-        musicas.append(musica.find_all('a')[0].get_text())
-    i+=1
+    i=1
+    while i<=10:
+        page = requests.get(f"https://www.last.fm/pt/music/Imagine+Dragons/+tracks?page={i}")
+        soup = BeautifulSoup(page.content, 'html.parser')
+        container_musicas =soup.find_all('td', class_='chartlist-name')
+        for musica in container_musicas:
+            musicas.append(musica.find_all('a')[0].get_text())
+        i+=1
