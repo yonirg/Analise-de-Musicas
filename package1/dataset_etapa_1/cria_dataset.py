ca137454b3aac6623d64944edfd6f924a912f557
yonirg <yonirabinovicigherman@gmail.com>
2022-10-07 18:23:17 -0300

    Converta dataframes importantes em csv para reutilização

--- package1/dataset_etapa_1/cria_dataset.py
+++ package1/dataset_etapa_1/cria_dataset.py
@@ -1,4 +1,5 @@
 import modulo_dataset as mds
+import pandas as pd
 
 dict_albuns_musicas = mds.albuns_musicas()
 
@@ -10,6 +11,7 @@ df_unicas = mds.pega_letras_unicas(dataframe)
 
 dataframe_com_letras = mds.letras_df(dataframe, df_unicas)
 
+
 dataframe_com_duracao = mds.duracao_df(dataframe_com_letras, df_unicas)
 
 dataframe_com_popularidade = mds.popularidade_df(dataframe_com_duracao, df_unicas)
@@ -17,10 +19,12 @@ dataframe_com_popularidade = mds.popularidade_df(dataframe_com_duracao, df_unica
 dataset = mds.apagar_colunas(dataframe_com_popularidade,["duration_msright", "Letraright"])
 dataset = dataset.rename(columns={"duration_ms":"Duracao(seg)"})
 dataset = dataset.rename(columns={"popularity":"Popularidade"})
-
-dataset_com_ouvintes = mds.ouvintes_por_album(dataset)
-
-print(dataset_com_ouvintes)
+dataset_final = mds.ouvintes_por_album(dataset)
+print(dataset_final)
 
 
+#USADO PARA CIRAR CSV's DOS DATAFRAMES E ACELERAR A UTILIZAÇÃO DAS FUNÇÕES DO PROJETO
 
+# dataframe.to_csv("datafame_vazio_multiindex.csv")
+# df_unicas.to_csv("dataframe_musicas_unicas.csv")
+# dataset_final.to_csv("dataset_final.csv")
