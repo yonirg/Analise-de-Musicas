42fd3ab9ef2554a8f5fab669e1bc59313b6ffd03
Lhc128 <lucashcc8@gmail.com>
2022-10-08 16:28:37 -0300

    adicione a função de quantidade de palavras das letras por album

--- package1/dataset_etapa_1/dataframes_prontos.py
+++ package1/dataset_etapa_1/dataframes_prontos.py
@@ -9,4 +9,3 @@ os.chdir(atual)
 dataset_com_ouvintes = pd.read_csv("dataset_final.csv", index_col=[0,1])
 dataframe = pd.read_csv("datafame_vazio_multiindex.csv", index_col=[0,1])
 df_unicas = pd.read_csv("dataset_final.csv", index_col=[0,1])
-
