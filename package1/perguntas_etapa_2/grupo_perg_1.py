11af8ee66de7ad8b96a5c3fc1d6dfe0db96223fe
yonirg <yonirabinovicigherman@gmail.com>
2022-10-08 01:38:14 -0300

    Corrija as respostas da pergunta 4 do grupo 1

--- package1/perguntas_etapa_2/grupo_perg_1.py
+++ package1/perguntas_etapa_2/grupo_perg_1.py
@@ -1,15 +1,18 @@
+from turtle import clear
 import funcoes as fun
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from dataframes_prontos import dataframe, dataset_com_ouvintes
+"""
 # Resposta da pergunta 1
 
 #Músicas com mais ouvintes por álbum:
+print("\n\n\n\nMúsicas com mais ouvintes por álbum\n\n\n\n")
 mais_ouvintes = fun.mais_ouvintes_por_album(dataset_com_ouvintes, dataframe)
 print(mais_ouvintes)
 
 #Músicas com menos ouvintes por álbum:
-
+print("\n\n\n\nMúsicas com menos ouvintes por álbum\n\n\n\n")
 menos_ouvintes = fun.menos_ouvintes_por_album(dataset_com_ouvintes, dataframe)
 print(menos_ouvintes)
 
@@ -21,12 +24,14 @@ print(menos_ouvintes)
 # Resposta da pergunta 2
 
 #Músicas com mais duração por álbum:
+print("\n\n\n\nMúsicas com mais duração por álbum\n\n\n\n")
 
 mais_longas = fun.menos_curtas_por_album(dataset_com_ouvintes, dataframe)
 print(mais_longas)
 
 
 #Músicas com menos duração por álbum:
+print("\n\n\n\nMúsicas com menos duração por álbum\n\n\n\n")
 
 mais_curtas = fun.mais_curtas_por_album(dataset_com_ouvintes, dataframe)
 print(mais_curtas)
@@ -34,23 +39,64 @@ print(mais_curtas)
 
 # Resposta da pergunta 3
 
-print("*****   TOP 5 MÚSICAS MAIS OUVIDAS    *****\n\n")
+print("\n\n\n\n*****   TOP 5 MÚSICAS MAIS OUVIDAS EM TODA A BANDA    *****")
+print("*****   AGUARDE UM MOMENTO, O GRÁFICO SERÁ EXIBIDO POR 6 SEGUNDOs E EM SEGUIDA SALVO NO DIRETÓRIO dataset_etapa_1    *****\n\n\n\n")
+
 mais_ouvida_geral = fun.musica_mais_tocada()
 
 
-print("*****   TOP 5 MÚSICAS MENOS OUVIDAS    *****\n\n")
+print("\n\n\n\n*****   TOP 5 MÚSICAS MENOS OUVIDAS    *****")
+print("*****   AGUARDE UM MOMENTO, O GRÁFICO SERÁ EXIBIDO POR 6 SEGUNDOS E EM SEGUIDA SALVO NO DIRETÓRIO dataset_etapa_1    *****\n\n\n\n")
+
 menos_ouvida_geral = fun.musica_menos_tocada()
 
 
 
 # Resposta da pergunta 4
+print("\n\n\n\nTOP 5 com menor duração de todas as músicas\n\n\n\n")
+
 dataframe_duracao = dataset_com_ouvintes.rename(columns={"Duracao(seg)":"Duracao"})
-dataframe_duracao = dataframe_duracao[dataframe_duracao.Duracao != "Nan"]
-dataframe_duracao_menor = dataframe_duracao['Duracao'].astype(int).sort_values(ascending=True)[:7]
-dataframe_duracao_menor = pd.DataFrame(dataframe_duracao_menor).reset_index().drop("Album", axis=1).drop_duplicates()
-print("TOP 5 com menor duração de todas as músicas\n\n")
-print(dataframe_duracao_menor)
-print("TOP 5 com maior duração de todas as músicas\n\n")
-dataframe_duracao_maior = dataframe_duracao['Duracao'].astype(int).sort_values(ascending=False)[:11]
-dataframe_duracao_maior = pd.DataFrame(dataframe_duracao_maior).reset_index().drop("Album", axis=1).drop_duplicates()
+dataframe_duracao_maior = dataframe_duracao.copy()
+dataframe_duracao_maior = dataframe_duracao_maior[dataframe_duracao_maior.Duracao != "Nan"]
+dataframe_duracao_maior = dataframe_duracao_maior.reset_index().drop("Album", axis=1)
+dataframe_duracao_maior['Duracao'] = dataframe_duracao_maior['Duracao'].astype(float)
+dataframe_duracao_maior = dataframe_duracao_maior.drop_duplicates().sort_values(by="Duracao", ascending=True)[:5]
+dataframe_duracao_maior.drop(["Letra", "Popularidade","Ouvintes"], axis=1, inplace=True)
+
 print(dataframe_duracao_maior)
+import matplotlib.pyplot as plt
+grafico_menor_duracao = dataframe_duracao_maior.plot(x="Musica", y="Duracao", kind="bar")
+grafico_menor_duracao.plot()
+plt.show(block=False)
+plt.pause(6)
+plt.close()
+plt.savefig("menor_duracao_geral")
+
+
+
+print("\n\n\n\nTOP 5 com maior duração de todas as músicas\n\n\n\n")
+print("*****   AGUARDE UM MOMENTO, O GRÁFICO SERÁ EXIBIDO POR 6 SEGUNDOS E EM SEGUIDA SALVO NO DIRETÓRIO dataset_etapa_1    *****\n\n\n\n")
+"""
+dataframe_duracao = dataset_com_ouvintes.rename(columns={"Duracao(seg)":"Duracao"})
+dataframe_duracao_maior = dataframe_duracao.copy()
+dataframe_duracao_maior = dataframe_duracao_maior[dataframe_duracao_maior.Duracao != "Nan"]
+dataframe_duracao_maior = dataframe_duracao_maior.reset_index().drop("Album", axis=1)
+dataframe_duracao_maior['Duracao'] = dataframe_duracao_maior['Duracao'].astype(float)
+dataframe_duracao_maior = dataframe_duracao_maior.drop_duplicates().sort_values(by="Duracao", ascending=False)[:6]
+dataframe_duracao_maior.drop(235, axis=0, inplace=True)
+dataframe_duracao_maior.drop(["Letra", "Popularidade","Ouvintes"], axis=1, inplace=True)
+"""
+print(dataframe_duracao_maior)
+grafico_menor_duracao = dataframe_duracao_maior.plot(x="Musica", y="Duracao", kind="bar")
+grafico_menor_duracao.plot()
+plt.show(block=False)
+plt.pause(6)
+plt.close()
+plt.savefig("maior_duracao_geral")
+"""
+
+import seaborn as sns
+df = dataframe_duracao_maior.drop(["Letra", "Popularidade","Ouvintes"], axis=1)
+mapa_cor = sns.heatmap(df.corr(), annot=True)
+plt.tight_layout()
+plt.show()
